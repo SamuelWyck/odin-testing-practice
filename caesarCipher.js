753d8f0f6c3a7ad99c9ceae2398d93d05fd92e4b
samuel <samawyck@gmail.com>
2025-03-25 14:01:11 -0500

    Create caesarCipher function

---
+++ caesarCipher.js
@@ -0,0 +1,30 @@
+
+
+function caesarChipher(string, offset) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const shiftedLetters = [];
+    const regex = /\w/;
+
+    for (let char of string) {
+        if (!regex.test(char)) {
+            shiftedLetters.push(char);
+            continue;
+        }
+
+        let index = alphabet.indexOf(char.toLowerCase());
+        index += offset;
+        index = (index > 25) ? index - 26 : index;
+        let newChar = alphabet[index];
+
+        if (char === char.toUpperCase()) {
+            newChar = newChar.toUpperCase();
+        }
+        
+        shiftedLetters.push(newChar);
+    }
+
+    return shiftedLetters.join("");
+};
+
+
+export default caesarChipher;
