c33ddd6c5a19ac6a173ede46c427325bfd79fe1d
samuel <samawyck@gmail.com>
2025-03-25 13:00:52 -0500

    Create tests for reverseString

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import {capitalize} from "./index.js";
+import {capitalize, reverseString} from "./index.js";
 
 it("capitalize exists", function() {
     expect(capitalize);
@@ -6,4 +6,12 @@ it("capitalize exists", function() {
 
 it("capitalize", function() {
     expect(capitalize("test")).toBe("Test");
+});
+
+it("reverseString exists", function() {
+    expect(reverseString);
+});
+
+it("reverseString works", function() {
+    expect(reverseString("test")).toBe("tset");
 });
